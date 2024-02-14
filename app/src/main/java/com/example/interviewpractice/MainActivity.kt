d90b1228608a55e3991b186ee39086fb9aca8b19
mapuntil <mapuntil@uwaterloo.ca>
2024-02-14 17:51:09 -0500

    Updated manifest

--- app/src/main/java/com/example/interviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/interviewpractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.interviewpractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.interviewpractice.ui.theme.InterviewPracticeTheme
 
 class MainActivity : ComponentActivity() {
+
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+            } else {
+
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContent {
             InterviewPracticeTheme {
                 // A surface container using the 'background' color from the theme
